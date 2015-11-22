d182d8588fffdabb54c946f7f0d3fd0a6c93b8a2
Dax T Games <dtgames@kinggeek.org>
2015-11-22 08:05:16 -0600

    Added Monokai

---
+++ base16-monokai.sh
@@ -0,0 +1,21 @@
+# solarized dark
+echo -ne '\e]11;#272822\a'    # 0   background
+echo -ne '\e]4;0;#01549e\a'   # 1/4 black
+echo -ne '\e]4;10;#74aa04\a'  # 2   bold green
+echo -ne '\e]4;11;#1a83a6\a'  # 3/6 bold yellow
+echo -ne '\e]4;9;#a70334\a'   # 4/1 bold red
+echo -ne '\e]4;13;#89569c\a'  # 5   bold magenta
+echo -ne '\e]4;12;#b6b649\a'  # 6/3 bold blue
+echo -ne '\e]4;7;#cacaca\a'   # 7   white (light grey really)
+echo -ne '\e]4;14;#7c7c7c\a'  # 8   bold cyan
+echo -ne '\e]4;4;#0383f5\a'   # 9   blue
+echo -ne '\e]4;2;#8dd006\a'   # 10  green
+echo -ne '\e]4;6;#58c2e5\a'   # 11  cyan
+echo -ne '\e]4;1;#f3044b\a'   # 12  red
+echo -ne '\e]4;5;#a87db8\a'   # 13  magenta
+echo -ne '\e]4;3;#cccc81\a'   # 14  yellow
+echo -ne '\e]4;15;#ffffff\a'  # 15  bold white
+# echo -ne '\e]4;8;#002b36\a'   # 0   bold black (i.e. dark grey)
+
+# echo -ne '\e]10;#eee8d5\a'    # 7 foreground
+# echo -ne '\e]12;#859900\a'    # 10 cursor
