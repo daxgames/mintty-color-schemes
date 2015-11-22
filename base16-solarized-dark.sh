fe6cbae2fe74583009b88e84d3359b7c7c0fa1bc
Dax T Games <dtgames@kinggeek.org>
2015-11-22 07:56:33 -0600

    changed to solarized and set order to match conemu

---
+++ base16-solarized-dark.sh
@@ -0,0 +1,22 @@
+# solarized dark
+echo -ne '\e]11;#002b36\a'    # 0   background
+echo -ne '\e]4;0;#073642\a'   # 1/4 black
+echo -ne '\e]4;10;#586e75\a'  # 2   bold green
+echo -ne '\e]4;11;#657b83\a'  # 3/6 bold yellow
+echo -ne '\e]4;9;#cb4b16\a'   # 4/1 bold red
+echo -ne '\e]4;13;#6c71c4\a'  # 5   bold magenta
+echo -ne '\e]4;12;#839496\a'  # 6/3 bold blue
+echo -ne '\e]4;7;#eee8d5\a'   # 7   white (light grey really)
+echo -ne '\e]4;14;#93a1a1\a'  # 8   bold cyan
+echo -ne '\e]4;4;#268bd2\a'   # 9   blue
+echo -ne '\e]4;2;#859900\a'   # 10  green
+echo -ne '\e]4;6;#2aa198\a'   # 11  cyan
+echo -ne '\e]4;1;#dc322f\a'   # 12  red
+echo -ne '\e]4;5;#d33682\a'   # 13  magenta
+echo -ne '\e]4;3;#b58900\a'   # 14  yellow
+echo -ne '\e]4;15;#fdf6e3\a'  # 15  bold white
+echo -ne '\e]4;8;#002b36\a'   # 0   bold black (i.e. dark grey)
+
+echo -ne '\e]10;#eee8d5\a'    # 7 foreground
+echo -ne '\e]12;#859900\a'    # 10 cursor
+
